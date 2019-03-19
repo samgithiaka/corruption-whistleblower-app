c2adaa8705c2f284b81615d6eb5ae8fceaa9a329
samuel muchemi githiaka <samgithiaka2013@gmail.com>
2019-03-19 19:33:16 +0300

    chat with smooch library

--- app/src/main/java/com/example/android/corruption_whistleblower_app/message.java
+++ app/src/main/java/com/example/android/corruption_whistleblower_app/message.java
@@ -7,15 +7,35 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
+import io.smooch.core.Settings;
+import io.smooch.core.Smooch;
+import io.smooch.core.SmoochCallback;
+import io.smooch.ui.ConversationActivity;
 
 public class message extends Activity{
     private FirebaseAuth mAuth;
+    String APP_ID ="5c9103ea04c4f900106749ae";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Smooch.init(getApplication(), new Settings(APP_ID), new SmoochCallback() {
+            @Override
+            public void run(Response response) {
+                // Handle init result
+            }
+        });
         setContentView(R.layout.activity_message);
 
+      Button  cht=findViewById(R.id.buttonChat);
+        cht.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                ConversationActivity.show(message.this, Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            }
+        });
 
           Button signout= findViewById(R.id.buttonAnonymousSignOut);
         signout.setOnClickListener(new View.OnClickListener() {
