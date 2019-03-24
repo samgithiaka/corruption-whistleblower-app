db94895d4c2b80bc9d2d6d2b05ba924aee1828c4
samuel muchemi githiaka <samgithiaka2013@gmail.com>
2019-03-24 15:36:02 +0300

    reporting

--- app/src/main/java/com/example/android/corruption_whistleblower_app/reportActivity.java
+++ app/src/main/java/com/example/android/corruption_whistleblower_app/reportActivity.java
@@ -1,13 +1,64 @@
 package com.example.android.corruption_whistleblower_app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
 
 public class reportActivity extends Activity {
-
+    private final int PICK_IMAGE_REQUEST = 71;
+    private Uri filePath;
+    private TextView imageView;
+    //Firebase
+    FirebaseStorage storage;
+    StorageReference storageReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+    Button attachImage= findViewById(R.id.buttonAttachImage);
+        attachImage.setOnClickListener(new View.OnClickListener() {
+//choosing the image
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+
+        }
+    })
+    ;
+}
+//set selected image filepath to string visible by user
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imageView = findViewById(R.id.imageView);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setText(filePath.toString());
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
