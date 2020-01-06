7c0816e646c84eb75f9bbb0c7b93a9c2c7950ca5
samuel muchemi githiaka <samgithiaka2013@gmail.com>
2020-01-06 13:50:43 +0300

    house cleaning and migrating to databinding

--- app/src/main/java/com/samgithiaka/corruption_whistleblower_app/MainActivity.java
+++ app/src/main/java/com/samgithiaka/corruption_whistleblower_app/MainActivity.java
@@ -2,85 +2,49 @@ package com.samgithiaka.corruption_whistleblower_app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
+
+import com.samgithiaka.corruption_whistleblower_app.databinding.ActivityMainBinding;
+import com.testfairy.TestFairy;
 
 import java.util.Objects;
 
 public class MainActivity extends BaseActivity {
     Toolbar myToolbar;
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TestFairy.begin(this, BuildConfig.TEST_FAIRY_KEY);
         setContentView(R.layout.activity_main);
 
-        TextView report = findViewById(R.id.report_corruption_text_view);
-        report.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Intent rpt = new Intent(MainActivity.this, reportActivity.class);
-                rpt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(rpt);
-            }
-        })
-        ;
-        TextView about = findViewById(R.id.about_text_view);
-        about.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Intent abt = new Intent(MainActivity.this, aboutActivity.class);
-                abt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(abt);
-            }
-        })
-        ;
-
-        TextView contact = findViewById(R.id.contact_us_text_view);
-        contact.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Intent cnt = new Intent(MainActivity.this, contact_us.class);
-                cnt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(cnt);
-            }
-        })
-        ;
-
-        TextView reports = findViewById(R.id.rec_view);
-        reports.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Intent rp = new Intent(MainActivity.this, PastReports.class);
-                rp.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(rp);
-            }
-        })
-        ;
-
-        TextView postbox = findViewById(R.id.postbox_text_view);
-        postbox.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Intent pstbox = new Intent(MainActivity.this, AnonymousAuthActivity.class);
-                pstbox.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(pstbox);
-            }
-        })
-        ;
+      binding.reportCorruptionTextView.setOnClickListener(v -> proceedToReporting());
+      binding.aboutTextView.setOnClickListener(v -> proceedToAbout());
+      binding.contactUsTextView.setOnClickListener(v -> proceedToContactUs());
+      binding.postboxTextView.setOnClickListener(v -> proceedToPostBox());
+      binding.recView.setOnClickListener(v -> proceedToPastReports());
     }
 
+    private void proceedToPastReports() {
+        startActivity(new Intent(MainActivity.this, PastReports.class));
+    }
+
+    private void proceedToPostBox() {
+        startActivity(new Intent(MainActivity.this, AnonymousAuthActivity.class));
+    }
+
+    private void proceedToContactUs() {
+        startActivity(new Intent(MainActivity.this, contact_us.class));
+    }
+
+    public void proceedToReporting() {
+         startActivity(new Intent(MainActivity.this, reportActivity.class));
+    }
+
+    public void proceedToAbout() {
+        startActivity(new Intent(MainActivity.this, aboutActivity.class));
+    }
     public void hideMainToolbar(boolean isHidden) {
         if (isHidden) {
             Objects.requireNonNull(getSupportActionBar()).hide();
